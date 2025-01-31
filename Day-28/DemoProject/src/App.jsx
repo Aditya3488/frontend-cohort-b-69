6297c9cfc78ec968ebf939644b947500d0b028fb
Aditya3488 <adityatiwari062@gmail.com>
2025-01-31 15:09:30 +0530

    Intro to React & Components

---
+++ Day-28/DemoProject/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card.jsx";
+function App() {
+  return (
+    <>
+      <h1 className="heading">Demo React Project Setup</h1>
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Day-28/DemoProject/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className='card'>
+      <img src='https://images.unsplash.com/photo-1524492412937-b28074a5d7da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kaWF8ZW58MHx8MHx8fDA%3D'  />
+      <h1>Taj Mahal</h1>
+      <p> lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quas</p>
+    </div>
+  )
+}
